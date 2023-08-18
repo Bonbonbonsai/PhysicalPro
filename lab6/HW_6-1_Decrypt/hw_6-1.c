7b79b11a0f7abaa3e3cfde69163fd910a1dc17b1
Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
2023-08-18 22:27:08 +0700

    hw_6-1

--- lab6/HW_6-1_Decrypt/hw_6-1.c
+++ lab6/HW_6-1_Decrypt/hw_6-1.c
@@ -2,26 +2,31 @@
 #include <string.h>
 #include <ctype.h>
 int main() {
-    char crypt[] = "ABXYPQRMNCEDKLJOSHTUFVZGWI";
+    char crypt[] = "ABXYPQRMNCEDKLJOSHTUFVZGWIABXYP";
     char string[200];
     char decrypt[200];
     scanf("%[^\n]", string);
     int string_len = strlen(string);
     int crypt_len = strlen(crypt);
     for (int i = 0; i < string_len; i++) {
-        if (isspace(string[i])) {
-            decrypt[i] = ' ';
+        if (!isalpha(string[i])) {
+            decrypt[i] = string[i];
         }
         else {
             char upper_letter = toupper(string[i]);
             for (int j = 0; j < crypt_len; j++) {
                 if (upper_letter == crypt[j]) {
-                    decrypt[i] = crypt[(j+5) % crypt_len];
+                    decrypt[i] = crypt[(j+5)];
                     break;
                 }
             }
+            if (islower(string[i])) {
+                decrypt[i] = tolower(decrypt[i]);
+            }
         }
     }
-    printf("%s", decrypt);
+    for (int k = 0; k < string_len; k++) {
+        printf("%c", decrypt[k]);
+    }
     return 0;
 }
