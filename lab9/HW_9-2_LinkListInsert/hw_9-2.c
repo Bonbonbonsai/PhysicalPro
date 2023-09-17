8dbff1ad49931f691583428af5e1ab76b77aa974
Sweet eyes <109953598+Bonbonbonsai@users.noreply.github.com>
2023-09-17 20:57:15 +0700

    link list

--- lab9/HW_9-2_LinkListInsert/hw_9-2.c
+++ lab9/HW_9-2_LinkListInsert/hw_9-2.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct node {
     int value;
     struct node *next;
 };
 
-void add(struct node * root, int value) {
-    struct node * pNew = (struct node *)malloc(sizeof(struct node));
+void add(struct node *root, int value) {
+    struct node *pNew = (struct node *)malloc(sizeof(struct node));
 
     pNew->value = value;
     pNew->next = NULL;
 
-    struct node * pointer = root;
+    struct node *pointer = root;
     while (pointer->next != NULL) {
         pointer = pointer->next;
     }
     pointer->next = pNew;
+}
+
+void insert(struct node *root, int position, int value) {
+    struct node *pNew = (struct node *)malloc(sizeof(struct node));
+    pNew->value = value;
+    
+    struct node *prev = root;
+    struct node *current = root->next;
+    int current_position = 0;
+    
+    while (current != NULL && current_position < position) {
+        prev = current;
+        current = current->next;
+        current_position++;
+    }
+    
+    pNew->next = current;
+    prev->next = pNew;
 }
 
 int main() {
@@ -27,9 +46,15 @@ int main() {
         scanf("%d", &value);
         add(&root, value);
     }
-    struct node * run = root.next;
+    
+    int insert_value, position;
+    scanf("%d %d", &insert_value, &position);
+    insert(&root, insert_value, position);
+
+    struct node *run = root.next;
     while (run != NULL) {
         printf("%d ", run->value);
         run = run->next;
     }
+    return 0;
 }
